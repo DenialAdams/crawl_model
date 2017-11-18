a3358b5eb7eb08a2fa3938993aef75c2eae95630
Richard McCormack <brick@brick.codes>
2017-11-18 18:10:07 -0500

    specify schema

--- src/db_schema.rs
+++ src/db_schema.rs
@@ -1,1 +1,23 @@
-infer_schema!("dotenv:DATABASE_URL");
+table! {
+    games (gid) {
+        gid -> Text,
+        name -> Text,
+        species_id -> BigInt,
+        background_id -> BigInt,
+        xl -> BigInt,
+        dam -> BigInt,
+        sdam -> BigInt,
+        tdam -> BigInt,
+        tmsg -> Text,
+        turn -> BigInt,
+        dur -> BigInt,
+        runes -> BigInt,
+        score -> BigInt,
+        start -> Timestamp,
+        end -> Timestamp,
+        potions_used -> BigInt,
+        scrolls_used -> BigInt,
+    }
+}
+
+joinable!(games -> species (species_id));
